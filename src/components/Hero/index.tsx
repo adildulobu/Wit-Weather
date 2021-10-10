dd93bc25c26dedb25f5c177af81789b512ec6c86
adildulobu <adildulobu@gmail.com>
2021-10-10 19:51:23 +0200

    Update index.tsx

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,18 +1,49 @@
 import React from "react";
 import { FiSearch } from 'react-icons/fi';
-import {HeroContainer, HeroContent, HeroBg,Input,Button} from './HeroElements'
+
+import {Formik,ErrorMessage} from 'formik';
+import * as yup from 'yup';
+
+import {HeroContainer, HeroContent, HeroBg,Input,Button, FormElement, InputSection,ErrorSection} from './HeroElements';
 
 function Hero(){
+    const Validationschema = yup.object().shape({
+        city: yup.string().required("City name is required")
+    })
     return(
         <HeroContainer>
             <HeroBg>
                 <HeroContent>
-                    <Input 
-                        placeholder="search city..."
-                    />
-                    <Button>
-                        <FiSearch />
-                    </Button>
+                            <Formik
+                                validationSchema = {Validationschema}
+                                initialValues= {{
+                                    city:''
+                                }}
+
+                                onSubmit={(values) =>{
+                                    alert(JSON.stringify(values))
+                                }}
+                            >
+                            {({handleSubmit,values,handleChange}) =>(
+                                <FormElement onSubmit={handleSubmit}>
+                                    <InputSection>
+                                        <Input 
+                                            id="city" 
+                                            name="city"
+                                            placeholder="search city..."
+                                            value={values.city}
+                                            onChange={handleChange}
+                                        />
+                                        <Button type="submit">
+                                            <FiSearch />
+                                        </Button>
+                                    </InputSection>
+                                    <ErrorSection>
+                                        <ErrorMessage name="city" />
+                                    </ErrorSection>
+                                </FormElement>
+                            )}
+                </Formik>
                 </HeroContent>
             </HeroBg>
         </HeroContainer>
