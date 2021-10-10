f16751ade215d14e0995720b642ace210fcc668e
adildulobu <adildulobu@gmail.com>
2021-10-10 18:26:34 +0200

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,35 @@
 import React from "react";
+
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
 import { FooterContainer,FooterText } from './FooterElements'
 
 function Footer(){
+
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyAdQVQrsk58qr44nH5bpVcx4DsPPVD-R_Y"
+      })
+
     return(
         <FooterContainer>
+
+            {
+                isLoaded ? (
+                    <GoogleMap
+                        mapContainerStyle={{width:'100%', height:'100%'}}
+                        center={{
+                            lat:-25.9740134,
+                            lng:32.5624548,
+                        }}
+                        zoom={15}
+                    >
+                      { /* Child components, such as markers, info windows, etc. */ }
+                      <></>
+                    </GoogleMap>
+                ) : <></>
+            }
+
             <FooterText>
                 Made By Adil Dulobu
             </FooterText>
